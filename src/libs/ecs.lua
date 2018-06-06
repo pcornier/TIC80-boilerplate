282e034e832fc73a0036e25ed8d11d9bcd865a91
Pierco <pier.cornier@gmail.com>
2018-06-06 12:28:50 +0200

    ECS refactoring

--- src/libs/ecs.lua
+++ src/libs/ecs.lua
@@ -6,51 +6,56 @@
 World = function()
  local systems = {}
  local entities = {}
- local world = {
-  addEntity = function(entity)
+ local world = {}
+
+ function world:addEntity(entity)
    entities[#entities+1] = entity
-  end,  
-  addSystem = function(system)
-   local function match(entity)
-    for _, component in pairs(system.filter) do
-     if entity[component] == nil then return false end
-    end
-    return true
+ end
+
+ function world:addSystem(system)
+  local function match(entity)
+   for _, component in pairs(system.filter) do
+    if entity[component] == nil then return false end
    end
-   systems[#systems+1] = function(entities, ...)
-    system.entities = {}
-    for _, entity in pairs(entities) do
-     if match(entity) then
-      system.entities[#system.entities+1] = entity
-     end
-    end
-    if system.update then system:update(...) end
-    if system.process then
-     for _, entity in pairs(system.entities) do
-      system:process(entity, ...)
-     end
-    end
-   end
-   system.world = world
-   if system.added then system:added() end
-  end, 
-  update = function(...)
-   for _, system in pairs(systems) do
-    system(entities, ...)
-   end
-  end,
-  removeEntity = function(entity)
-   for i,e in pairs(entities) do
-    if e == entity then
-     entities[i] = nil
-     break
-    end
-   end
-  end,
-  getEntities = function()
-   return entities
+   return true
   end
- }
+  systems[#systems+1] = function(entities, ...)
+   system.entities = {}
+   for _, entity in pairs(entities) do
+    if match(entity) then
+     system.entities[#system.entities+1] = entity
+    end
+   end
+   if system.update then system:update(...) end
+   if system.process then
+    for _, entity in pairs(system.entities) do
+     system:process(entity, ...)
+    end
+   end
+  end
+  system.world = world
+  if system.added then system:added() end
+ end
+
+ function world:update(...)
+  for _, system in pairs(systems) do
+   system(entities, ...)
+  end
+ end
+
+ function world:removeEntity(entity)
+  for i,e in pairs(entities) do
+   if e == entity then
+    entities[i] = nil
+    break
+   end
+  end
+ end
+
+ function world:getEntities()
+  return entities
+ end
+
  return world
 end
 
