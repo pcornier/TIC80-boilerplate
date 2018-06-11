d6bf824c3387dad57158838c867dda010b3836eb
Pierre Cornier <pier.cornier@gmail.com>
2018-06-11 17:18:37 +0200

    now we can filter entities manually

--- src/libs/ecs.lua
+++ src/libs/ecs.lua
@@ -8,21 +8,22 @@ World = function()
  local entities = {}
  local world = {}
 
+ function match(filter, entity)
+  for _, component in pairs(filter) do
+   if entity[component] == nil then return false end
+  end
+  return true
+ end
+
  function world:addEntity(entity)
    entities[#entities+1] = entity
  end
 
  function world:addSystem(system)
-  local function match(entity)
-   for _, component in pairs(system.filter) do
-    if entity[component] == nil then return false end
-   end
-   return true
-  end
   systems[#systems+1] = function(entities, ...)
    system.entities = {}
    for _, entity in pairs(entities) do
-    if match(entity) then
+    if match(system.filter, entity) then
      system.entities[#system.entities+1] = entity
     end
    end
@@ -35,6 +36,16 @@ World = function()
   end
   system.world = world
   if system.added then system:added() end
+ end
+
+ function world:filter(filter)
+  matches = {}
+  for _, entity in pairs(entities) do
+   if match(filter, entity) then
+    matches[#matches+1] = entity
+   end
+  end
+  return matches
  end
 
  function world:update(...)
